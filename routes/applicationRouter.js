5f57db705f50f883be3665a961b665aa3c478509
Mark Cheli <mpcheli7@gmail.com>
2016-09-25 19:52:54 -0400

    Fixed get application by ID endpoint

--- routes/applicationRouter.js
+++ routes/applicationRouter.js
@@ -50,13 +50,8 @@ applicationRouter.route('/:applicationId')
 //Only if user has access to application
     .get(Verify.verifyOrdinaryUser, Verify.verifyAuthorized, function (req, res, next) {
         Applications.findById(req.params.applicationId, function (err, application) {
-            // var checkAuthorized = Verify.verifyAuthorized(req, application.authorizedUsers);
-            // if(checkAuthorized.authorized){
-            //     res.json(application);
-            // }else{
-            //     res.json(checkAuthorized.error)
-            // }
-
+            if (err) throw err;
+            res.json(application);
         });
     })
 
