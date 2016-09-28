8cebd21e29ffb23253a014a8568c22667f8a1c74
Mark Cheli <mpcheli7@gmail.com>
2016-09-28 11:08:15 -0400

    Updated postinstall to include bower

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://admin:admin@ds041486.mlab.com:41486/admin'
+    'mongoUrl': 'mongodb://admin:admin@ds041486.mlab.com:41486/heroku_7dhx8svk'
 }
