ff0ab81b3d3eec2298ce36da4ac85d6856376466
Mark Cheli <mpcheli7@gmail.com>
2016-09-30 12:41:35 -0400

    Sidebar now shows active applicaiton as highlighted

--- public/scripts/controllers/MainController.js
+++ public/scripts/controllers/MainController.js
@@ -2,9 +2,28 @@
 
 angular.module('CSM')
 
-    .controller('MainController', ['$scope', '$rootScope', 'applicationFactory', function ($scope, $rootScope, applicationFactory) {
+    .controller('MainController', ['$scope', '$rootScope', '$state', 'applicationFactory', function ($scope, $rootScope, $state, applicationFactory) {
 
         $scope.applications = applicationFactory.query();
+        $scope.activeApplication = "";
+        $scope.active = false;
+
+        $scope.applications.$promise.then(function (data) {
+            $scope.activeApplication = data[0].applicationName;
+            $state.go('main.application', {name: data[0]._id})
+        });
+
+        $scope.setActive = function (name) {
+            $scope.activeApplication = name;
+        }
+
+        $scope.evalActive = function (name) {
+            if (name == $scope.activeApplication) {
+                return true;
+            } else {
+                return false;
+            }
+        }
 
 
     }])
